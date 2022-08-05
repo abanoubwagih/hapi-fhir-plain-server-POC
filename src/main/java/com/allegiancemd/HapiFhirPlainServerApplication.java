4af336e457bc48c771f968bc089ffe03c1794533
HishamGhander <hisham011232@ci.suez.edu.eg>
2022-08-05 10:34:37 +0200

    injected patientService to HapiFhirPlainServerApplication

--- src/main/java/com/allegiancemd/HapiFhirPlainServerApplication.java
+++ src/main/java/com/allegiancemd/HapiFhirPlainServerApplication.java
@@ -1,13 +1,17 @@
 package com.allegiancemd;
 
 import com.allegiancemd.fhir.SimpleRestfulServer;
+import com.allegiancemd.service.PatientService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class HapiFhirPlainServerApplication {
+    private final PatientService patientService;
 
     public static void main(String[] args) {
         SpringApplication.run(HapiFhirPlainServerApplication.class, args);
@@ -15,7 +19,7 @@ public class HapiFhirPlainServerApplication {
 
     @Bean
     public ServletRegistrationBean ServletRegistrationBean() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new SimpleRestfulServer(), "/*");
+        ServletRegistrationBean registration = new ServletRegistrationBean(new SimpleRestfulServer(patientService), "/*");
         registration.setName("FhirServlet");
         return registration;
     }
