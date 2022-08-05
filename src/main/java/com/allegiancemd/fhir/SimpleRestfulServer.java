9038005cc9de65e0ec815eed5c183e061f061bb7
HishamGhander <hisham011232@ci.suez.edu.eg>
2022-08-05 10:33:56 +0200

    added PatientResourceProvider to SimpleRestfulServer

--- src/main/java/com/allegiancemd/fhir/SimpleRestfulServer.java
+++ src/main/java/com/allegiancemd/fhir/SimpleRestfulServer.java
@@ -2,16 +2,23 @@ package com.allegiancemd.fhir;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.server.RestfulServer;
+import com.allegiancemd.resourceprovider.PatientResourceProvider;
+import com.allegiancemd.service.PatientService;
+import lombok.RequiredArgsConstructor;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
+@RequiredArgsConstructor
 @WebServlet("/*")
 public class SimpleRestfulServer extends RestfulServer {
+    private final PatientService patientService;
+
     @Override
     protected void initialize()throws ServletException {
         //create a context for the appropriate version
         setFhirContext(FhirContext.forR4());
-        //Register Resource Providers - COMING SOON
+        //Register Resource Providers
+        registerProvider(new PatientResourceProvider(patientService));
     }
 }
