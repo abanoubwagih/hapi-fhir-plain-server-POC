ec8d897a22015838777efecb260f2f50b1220e14
HishamGhander <hisham011232@ci.suez.edu.eg>
2022-08-05 03:41:23 +0200

    created ServletRegistrationBean

--- src/main/java/com/allegiancemd/HapiFhirPlainServerApplication.java
+++ src/main/java/com/allegiancemd/HapiFhirPlainServerApplication.java
@@ -1,13 +1,23 @@
 package com.allegiancemd;
 
+import com.allegiancemd.fhir.SimpleRestfulServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HapiFhirPlainServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HapiFhirPlainServerApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean ServletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new SimpleRestfulServer(), "/*");
+        registration.setName("FhirServlet");
+        return registration;
     }
 
 }
